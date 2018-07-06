47977fa343799073ead4e687d7c64e94caf13754
Askhat <serikov.askhat@gmail.com>
2018-07-06 20:50:26 +0600

    initial commit

---
+++ Converters/ImageConverter/MyImageConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Converters.Image;
+using ConvertInterfaces;
+
+namespace Converters.ImageConverter
+{
+    public abstract class MyImageConverter<T> where T : IMyImage
+    {
+        public string Name { get; protected set; }
+        public int NumberOfParams { get; protected set; }
+        public Type TypeOfParams { get; protected set; }
+        public int ConverterType { get; protected set; }
+        protected MyImageConverter()
+        {
+            NumberOfParams = 0;
+            TypeOfParams = null;
+            Name = "Empty";
+        }
+        protected virtual int Norm(double x)
+        {
+            return x > 255 ? 255 : (int)x;
+        }
+
+    }
+}
